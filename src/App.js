9ec8454bf1d04ff15f7a6924d631a7d5c7a09b4f
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-05-07 04:15:07 +0600

    Navbar Addedd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './components/Home/Home/Home';
 
 function App() {
   return (
     <div>
-      
+      <Home/>
     </div>
   );
 }
--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
-
+import logo from '../../Image/logo.png';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import './Header.css';
 const Header = () => {
     return (
         <div>
-            
+            <Navbar className="" collapseOnSelect expand="lg">
+                <Navbar.Brand to="/home"><img className="logo" src={logo} alt="" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto navbar m-3 p-3">
+                        <Nav.Link to="/home">HOME</Nav.Link>
+                        <NavDropdown title="DONOR LIST" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#A+">A+</NavDropdown.Item>
+                            <NavDropdown.Item href="#A-">A-</NavDropdown.Item>
+                            <NavDropdown.Item href="#B+">B+</NavDropdown.Item>
+                            <NavDropdown.Item href="#B-">B-</NavDropdown.Item>
+                            <NavDropdown.Item href="#O+">O+</NavDropdown.Item>
+                            <NavDropdown.Item href="#O-">O-</NavDropdown.Item>
+                            <NavDropdown.Item href="#AB+">AB+</NavDropdown.Item>
+                            <NavDropdown.Item href="#AB-">AB-</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        </NavDropdown>
+                        <NavDropdown title="BE A DONOR" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#A+">A+</NavDropdown.Item>
+                            <NavDropdown.Item href="#A-">A-</NavDropdown.Item>
+                            <NavDropdown.Item href="#B+">B+</NavDropdown.Item>
+                            <NavDropdown.Item href="#B-">B-</NavDropdown.Item>
+                            <NavDropdown.Item href="#O+">O+</NavDropdown.Item>
+                            <NavDropdown.Item href="#O-">O-</NavDropdown.Item>
+                            <NavDropdown.Item href="#AB+">AB+</NavDropdown.Item>
+                            <NavDropdown.Item href="#AB-">AB-</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        </NavDropdown>
+                        <Nav.Link to="/work">WORK</Nav.Link>
+                        <Nav.Link to="/rules">RULES</Nav.Link>
+                        <Nav.Link to="/blog">BLOG</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 };
