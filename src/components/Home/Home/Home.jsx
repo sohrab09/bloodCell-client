901849ee8d3997ebfae745f4e90540b72ddb1b30
Sohrab Hossain <sohrab.cse9@gmail.com>
2021-06-12 18:39:52 +0600

    Importance add

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Feature from '../Feature/Feature';
+import Importance from '../Importance/Importance';
+
 const Home = () => {
     return (
         <div>
             <Header/>
             <Feature />
+            <Importance />
         </div>
     );
 };
